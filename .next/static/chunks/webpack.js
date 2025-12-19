fdb7f454a9cd4c9524505b82b2357fe4da911458
briankabugi <briannjau01@gmail.com>
2025-12-19 23:00:04 +0300

    Stable 2; Start campaign with contact segments and display but campaigns not persistent

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e19c598fc1073b0d"; }
+/******/ 		__webpack_require__.h = function() { return "824f4291c9c101cc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
